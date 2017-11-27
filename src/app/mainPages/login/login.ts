4c1e93caef323eece5d5350fc29a65dabf689ed5
Long <1300578660@qq.com>
2017-11-27 22:35:25 +0800

    update login tips

--- src/app/mainPages/login/login.ts
+++ src/app/mainPages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NzModalService , NzMessageService } from 'ng-zorro-antd';
 import * as md5 from 'md5';
 
 import { GetDataService } from '../../library/getDataService/getDataService';
@@ -12,19 +13,29 @@ export class LoginComponent {
     public user_name = '';
     public password = '';
 
-    constructor(private dataService: GetDataService, private router: Router) {}
+    constructor(
+        private dataService: GetDataService,
+        private router: Router,
+        private _message: NzMessageService
+    ) {}
 
     public SignIn() {
         const data = {
             name : this.user_name,
             password : md5(this.password)
         };
+        let _id = this._message.loading("正在登录，请稍等...").messageId;
         this.dataService.postData('/index/index/login', data, false).then(res => {
-            if (res.status === 0) {
+            if (res.status === "success") {
+                this._message.remove(_id);
+                this._message.success("登录成功",{nzDuration : 500});
+                setTimeout(()=>{
+                    this.router.navigate(['/home']);
+                },500);
                 sessionStorage.setItem('token', res.data.token);
-                this.router.navigate(['/home']);
             }else {
-                alert('用户名或密码错');
+                this._message.remove(_id);
+                this._message.error(res.msg);
             }
         });
     }
